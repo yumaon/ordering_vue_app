b86afcb5de620ee3605f0bf71557dae4d4d52591
yumaon <110388364+yumaon@users.noreply.github.com>
2024-10-14 16:55:23 +0900

    削除機能の実装

--- ordering_vue_app/resources/js/Pages/Products/Index.vue
+++ ordering_vue_app/resources/js/Pages/Products/Index.vue
@@ -1,11 +1,22 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import DangerButton from '@/Components/DangerButton.vue';
 import { Head } from '@inertiajs/vue3';
-import { Link } from '@inertiajs/vue3';
+import { Link, useForm } from '@inertiajs/vue3';
 
 const props = defineProps({
   products: {type:Object}
 });
+
+const form = useForm({
+  id: '',
+});
+
+const deleteProduct = (id, name) => {
+  if(confirm("Are you sure to delete " + name + "?")) {
+    form.delete(route('products.destroy', id));
+  }
+};
 </script>
 
 <template>
@@ -57,7 +68,11 @@ const props = defineProps({
                           <i class="fa-solid fa-edit"></i> 
                         </Link>
                       </td>
-                      <td class="border border-gray-400 px-4 py-2 text-center"></td>
+                      <td class="border border-gray-400 px-4 py-2 text-center">
+                        <DangerButton @click="deleteProduct(product.id, product.name)">
+                          <i class="fa-solid fa-trash"></i>
+                        </DangerButton>
+                      </td>
                   </tr>
                 </tbody>
               </table>   
