c941b78f8e086eeff40052f08d06addbfea540e1
yumaon <110388364+yumaon@users.noreply.github.com>
2024-10-18 17:22:15 +0900

    消費税額（通常税率・軽減税率）の表示

--- ordering_vue_app/app/Http/Controllers/OrderController.php
+++ ordering_vue_app/app/Http/Controllers/OrderController.php
@@ -12,6 +12,7 @@ use Inertia\Inertia;
 use App\Http\Resources\OrderResource;
 
 use Illuminate\Http\Request;
+use NumberFormatter;
 
 class OrderController extends Controller
 {
@@ -76,9 +77,53 @@ class OrderController extends Controller
     public function show(Order $order)
     {
         $orderResource = new OrderResource($order);
+
+        $normal_tax_total = 0;
+        $reduced_tax_total = 0;
+        // 商品1
+        if($orderResource->product1['tax'] == 10) {
+            $normal_tax_total = $normal_tax_total + $orderResource->product1['price'] * $orderResource->num1;
+        }
+        if($orderResource->product1['tax'] == 8) {
+            $reduced_tax_total = $reduced_tax_total + $orderResource->product1['price'] * $orderResource->num1;
+        }
+        // 商品2
+        if($orderResource->product2) {
+            if($orderResource->product2['tax'] == 10) {
+                $normal_tax_total = $normal_tax_total + $orderResource->product2['price'] * $orderResource->num2;
+            }
+            if($orderResource->product2['tax'] == 8) {
+                $reduced_tax_total = $reduced_tax_total + $orderResource->product2['price'] * $orderResource->num2;
+            }
+        }
+        
+        // 商品3
+        if($orderResource->product3) {
+            if($orderResource->product3['tax'] == 10) {
+                $normal_tax_total = $normal_tax_total + $orderResource->product3['price'] * $orderResource->num3;
+            }
+            if($orderResource->product3['tax'] == 8) {
+                $reduced_tax_total = $reduced_tax_total + $orderResource->product3['price'] * $orderResource->num3;
+            }
+        }
+
+        // 税率計算
+        $normal_tax = (int)($normal_tax_total * 0.1);
+        $reduced_tax = (int)($reduced_tax_total * 0.08);
+        $total = $normal_tax_total + $reduced_tax_total + $normal_tax + $reduced_tax;
+
         return Inertia::render('Orders/Show', [
             'order' => $orderResource,
+            'normal_tax_total' => number_format($normal_tax_total),
+            'reduced_tax_total' => number_format($reduced_tax_total),
+            'normal_tax' => number_format($normal_tax),
+            'reduced_tax' => number_format($reduced_tax),
+            'total' => number_format($total)
         ]);
+        
+        // return Inertia::render('Orders/Show', [
+        //     'order' => $orderResource,
+        // ]);
     }
 
     /**
--- ordering_vue_app/resources/js/Pages/Orders/Show.vue
+++ ordering_vue_app/resources/js/Pages/Orders/Show.vue
@@ -6,9 +6,13 @@ import InputLabel from "@/Components/InputLabel.vue";
 import PrimaryButton from "@/Components/PrimaryButton.vue";
 
 const props = defineProps({
-  order: { tyoe: Object },
+  order: { type: Object },
+  total: "",
+  normal_tax_total: "",
+  reduced_tax_total: "",
+  normal_tax: "",
+  reduced_tax: "",
 });
-
 const handleSubmit = () => {};
 console.log(props);
 </script>
@@ -38,120 +42,134 @@ console.log(props);
         </div>
 
         <form @submit.prevent="submit" class="w-3/4">
-          <div class="">
-            <InputLabel for="order_id" value="ID" />
-            <div class="ml-2 font-semibold text-xl">{{ props.order.data.id }}</div>
+          <div class="flex">
+            <div class="w-32">
+              <InputLabel for="order_id" value="ID" />
+              <div class="ml-2 font-semibold text-xl">{{ props.order.data.id }}</div>
+            </div>
+            <div class="w-72">
+              <InputLabel for="order_id" value="注文日" />
+              <div class="ml-2 font-semibold text-xl">{{ props.order.data.orderday }}</div>
+            </div>
+            <div class="">
+              <InputLabel for="customer_id" value="客先" />
+              <div class="ml-2 font-semibold text-xl">{{ props.order.data.customer.name }}</div>
+            </div>
           </div>
-          <div class="mt-4">
-            <InputLabel for="order_id" value="注文日" />
-            <div class="ml-2 font-semibold text-xl">{{ props.order.data.orderday }}</div>
-          </div>
-          <div class="mt-4">
-            <InputLabel for="customer_id" value="客先" />
-            <div class="ml-2 font-semibold text-xl">{{ props.order.data.customer.name }}</div>
-          </div>
-          <!--ここから商品1-->
+
+          <!--ここから小計-->
           <div class="mt-4 flex">
-            <div>
-              <InputLabel for="product_id1" value="商品1" />
-              <div class="ml-2 font-semibold text-xl w-40">
-                {{ props.order.data.product1.name }}
-              </div>
-            </div>
-            <div class="flex">
-              <div class="ml-10 w-24">
-                <InputLabel value="コード" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product1.code }}
-                </div>
-              </div>
-              <div class="ml-10 w-24">
-                <InputLabel value="価格" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product1.price }}円
-                </div>
-              </div>
-              <div class="ml-10 w-24">
-                <InputLabel value="税率" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product1.tax }}%
-                </div>
-              </div>
-            </div>
-            <div class="ml-10">
-              <InputLabel for="num1" value="注文数" />
-              <div class="mt-3 ml-2 font-medium text-xl">{{ props.order.data.num1 }}%</div>
-            </div>
+            <table class="table-auto border border-gray-400 w-10/12">
+              <thead>
+                <tr class="bg-blue-100">
+                  <th class="px-4 py-2"></th>
+                  <th class="px-4 py-2 text-xs">お買い上げ額合計（円）</th>
+                  <th class="px-4 py-2 text-xs">消費税額合計（円）</th>
+                  <th class="px-4 py-2 text-xs">ご請求金額（円）</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white">
+                  <td class="border border-gray-400 px-4 py-2 text-center">通常税率（10%）</td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.normal_tax_total }}円
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.normal_tax }}円
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center" rowspan="2">
+                    {{ props.total }}円
+                  </td>
+                </tr>
+                <tr class="bg-white">
+                  <td class="border border-gray-400 px-4 py-2 text-center">軽減税率（ 8%）</td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.reduced_tax_total }}円
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.reduced_tax }}円
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <!--ここまで商品1-->
-          <!--ここから商品2-->
-          <div v-if="props.order.data.product2?.name" class="mt-4 flex">
-            <div>
-              <InputLabel for="product_id2" value="商品2" />
-              <div class="ml-2 font-semibold text-xl w-40">
-                {{ props.order.data.product2.name }}
-              </div>
-            </div>
-            <div class="flex">
-              <div class="ml-10 w-24">
-                <InputLabel value="コード" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product2.code }}
-                </div>
-              </div>
-              <div class="ml-10 w-24">
-                <InputLabel value="価格" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product2.price }}円
-                </div>
-              </div>
-              <div class="ml-10 w-24">
-                <InputLabel value="税率" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product2.tax }}%
-                </div>
-              </div>
-            </div>
-            <div class="ml-10">
-              <InputLabel for="num1" value="注文数" />
-              <div class="mt-3 ml-2 font-medium text-xl">{{ props.order.data.num2 }}%</div>
-            </div>
+          <!--ここまで小計-->
+
+          <!--ここから明細-->
+          <div class="mt-4">
+            <InputLabel for="" value="明細" />
           </div>
-          <!--ここまで商品2-->
-          <!--ここから商品3-->
-          <div v-if="props.order.data.product3?.name" class="mt-4 flex">
-            <div>
-              <InputLabel for="product_id3" value="商品3" />
-              <div class="ml-2 font-semibold text-xl w-40">
-                {{ props.order.data.product3.name }}
-              </div>
-            </div>
-            <div class="flex">
-              <div class="ml-10 w-24">
-                <InputLabel value="コード" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product3.code }}
-                </div>
-              </div>
-              <div class="ml-10 w-24">
-                <InputLabel value="価格" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product3.price }}円
-                </div>
-              </div>
-              <div class="ml-10 w-24">
-                <InputLabel value="税率" />
-                <div class="mt-3 ml-2 font-medium text-xl">
-                  {{ props.order.data.product3.tax }}%
-                </div>
-              </div>
-            </div>
-            <div class="ml-10">
-              <InputLabel for="num1" value="注文数" />
-              <div class="mt-3 ml-2 font-medium text-xl">{{ props.order.data.num3 }}%</div>
-            </div>
+          <div class="mt-4 flex">
+            <table class="table-auto border border-gray-400 w-10/12">
+              <thead>
+                <tr class="bg-blue-100">
+                  <th class="px-4 py-2">商品</th>
+                  <th class="px-4 py-2 text-xs">コード</th>
+                  <th class="px-4 py-2 text-xs">価格</th>
+                  <th class="px-4 py-2 text-xs">税率</th>
+                  <th class="px-4 py-2 text-xs">注文数</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class="bg-white">
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product1.name }}
+                    <span v-if="props.order.data.product1.tax == 8">＊</span>
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product1.code }}
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product1.price }}円
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product1.tax }}％
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.num1 }}
+                  </td>
+                </tr>
+                <tr v-if="props.order.data.product2?.name" class="bg-white">
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product2.name }}
+                    <span v-if="props.order.data.product2.tax == 8">＊</span>
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product2.code }}
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product2.price }}円
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product2.tax }}％
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.num2 }}
+                  </td>
+                </tr>
+                <tr v-if="props.order.data.product3?.name" class="bg-white">
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product3.name }}
+                    <span v-if="props.order.data.product3.tax == 8">＊</span>
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product3.code }}
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product3.price }}円
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.product3.tax }}％
+                  </td>
+                  <td class="border border-gray-400 px-4 py-2 text-center">
+                    {{ props.order.data.num3 }}
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-          <!--ここまで商品3-->
+          <div>＊軽減税率対象</div>
+          <!--ここまで明細-->
 
           <div class="flex items-center mt-8 mb-8">
             <PrimaryButton class="ms-4" @click="handleSubmit">請求書発行</PrimaryButton>
